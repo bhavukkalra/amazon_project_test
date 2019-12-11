3939f022d5cd2f2fa6a95d225afddf90599c11f3
Bhavuk kalra <41142068+bhavukkalra@users.noreply.github.com>
2019-12-11 19:07:20 +0530

    Update amazon_project.py

--- amazon_project.py
+++ amazon_project.py
@@ -29,14 +29,15 @@ def send_mail():
     server.ehlo()
     server.starttls()
     server.ehlo()
-    server.login('bhavukkalra1786@gmail.com','your_gmail_authentication_password')
+    server.login('YOUR_EMAIL','your_gmail_authentication_password')
     #kind of temporary password to access your gmail mailing service
     
     subject = 'MakerSpace Project'
     body = 'check this thing quickly https://www.amazon.in/Apple-MacBook-13-inch-1-8GHz-Dual-core/dp/B073Q5R6VR?tag=googinhydr18418-21&tag=googinkenshoo-21&ascsubtag=_k_CjwKCAjw1f_pBRAEEiwApp0JKIrQZ1qcvacwVsuTrnbMIdZWdrmyq4b8qLjNvJmKarbLPylGbRU5YBoCHLsQAvD_BwE_k_&gclid=CjwKCAjw1f_pBRAEEiwApp0JKIrQZ1qcvacwVsuTrnbMIdZWdrmyq4b8qLjNvJmKarbLPylGbRU5YBoCHLsQAvD_BwE'
     msg = f"Subject: {subject}\n\n\n{body}"
     
-    server.sendmail('bhavukkalra1786@gmail.com','manju198bala@gmail.com',msg)
+    server.sendmail('SENDERS_EMAIL','RECEIVERS_EMAIL',msg)
+    #SENDERS_ADDRESS could be different from your email address
     print('hey email has been sent')
     server.quit()
 
